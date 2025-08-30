1d2e05da272d3b50f81e526fd95842a274c3d49d
Anton Tur <ant.tur85@gmail.com>
2025-08-30 14:33:40 +0300

    add routes

--- index.js
+++ index.js
@@ -1,37 +1,109 @@
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
 
-const products = [
+app.use(cors());
+app.use(express.json());
+
+morgan.token('body', req => {
+  if (req.method === 'POST' || req.method === 'PUT') {
+    return JSON.stringify(req.body);
+  }
+
+  return '';
+});
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+let products = [
   {
-    id: 'f780',
+    id: '1',
     name: 'Water',
     amount: 1,
     checked: false,
   },
   {
-    id: '1815',
+    id: '2',
     name: 'Bread',
     amount: 1,
     checked: false,
   },
   {
-    id: '086d',
+    id: '3',
     name: 'eggs',
     amount: 20,
     checked: false,
   },
   {
-    id: '5797',
+    id: '4',
     name: 'hello',
     amount: 1,
     checked: false,
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(products));
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.get('/api/products', (request, response) => {
+  response.json(products);
+});
+
+app.get('/api/products/:id', (request, response) => {
+  const id = request.params.id;
+  const product = products.find(prod => prod.id === id);
+
+  if (product) {
+    response.json(product);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/products/:id', (request, response) => {
+  const id = request.params.id;
+  products = products.filter(prod => prod.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    products.length > 0 ? Math.max(...products.map(n => Number(n.id))) : 0;
+
+  return String(maxId + 1);
+};
+
+app.post('/api/products', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'product name missing',
+    });
+  }
+
+  product = {
+    name: body.name,
+    checked: body.checked || false,
+    amount: body.amount,
+    id: generateId(),
+  };
+
+  products = products.concat(product);
+  response.json(product);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
